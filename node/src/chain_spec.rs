562265291328981dc5414817fd2aed87685e2d29
lost-inwind <lostinwind@protonmail.com>
2023-06-16 18:19:56 +0800

    staging_testnet

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -5,7 +5,7 @@ use matrix_ai_runtime::{
 use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_consensus_grandpa::AuthorityId as GrandpaId;
-use sp_core::{sr25519, Pair, Public};
+use sp_core::{crypto::UncheckedInto, sr25519, Pair, Public};
 use sp_runtime::traits::{IdentifyAccount, Verify};
 
 // The URL for the telemetry server.
@@ -123,6 +123,58 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 		// Properties
 		None,
 		None,
+		// Extensions
+		None,
+	))
+}
+
+pub fn staging_testnet_config() -> Result<ChainSpec, String> {
+	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
+
+	Ok(ChainSpec::from_genesis(
+		// Name
+		"Staging Testnet",
+		// ID
+		"staging_testnet",
+		ChainType::Live,
+		move || {
+			testnet_genesis(
+				wasm_binary,
+				// Initial PoA authorities
+				vec![
+					(	
+						// 5DFFyx4A49ZJ59NEWBNybRMzKpHCMpvwMu8ep5sYyxuUt5Zw
+						array_bytes::hex2array_unchecked("3448d4ea6b08145500c236a90c0e50ddd2f216e6c6851b10e3e42defc3d33344")
+							.unchecked_into(),
+						// 5HBEF4fsh8vDqqXSU5YveGy3DgXteVBUBDNpwHA8cXdKrAVY
+						array_bytes::hex2array_unchecked("2eea428d7d044348c4509aef9fcdd8b9bed90aaf22b10e4116221a1c4c56d318")
+							.unchecked_into(),
+					),
+				],
+				// Sudo account
+				// 5DCrHRqGim8Cpqj8ww4Z7GjK2LQLFKJKDbonqcSnmi5Q3KhH
+				array_bytes::hex_n_into_unchecked("3272906cf2832a98f83dd64ac89213201d3d37bde3b44ffd8a630d0523a00878"),
+				// Pre-funded accounts
+				vec![
+					// 5DCrHRqGim8Cpqj8ww4Z7GjK2LQLFKJKDbonqcSnmi5Q3KhH
+					array_bytes::hex_n_into_unchecked("3272906cf2832a98f83dd64ac89213201d3d37bde3b44ffd8a630d0523a00878"),
+				],
+				true,
+			)
+		},
+		// Bootnodes
+		vec![],
+		// Telemetry
+		None,
+		// Protocol ID
+		Some("MatrixAI"),
+		None,
+		// Properties
+		Some(
+			serde_json::from_str(
+				"{\"tokenDecimals\": 12, \"tokenSymbol\": \"MAI\"}",
+			).expect("Provided valid json map"),
+		),
 		// Extensions
 		None,
 	))
